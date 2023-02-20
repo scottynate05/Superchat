0f02ed5bc13e5197024a34787ea431798310a7fe
Nathan Brown <scottynate05@yahoo.com>
2023-02-20 15:06:39 -0800

    added hooks

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
 function App() {
   return (
     <div className="App">
