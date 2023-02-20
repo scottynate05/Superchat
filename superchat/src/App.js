1b83be8314c42489c5be3e52065dda75fab86b7d
Nathan Brown <scottynate05@yahoo.com>
2023-02-20 15:33:00 -0800

    Established firebase connection

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -8,6 +8,21 @@ import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
+firebase.initializeApp({
+  apiKey: "AIzaSyCtsyYLKRXANTAJ38GbU3-CCa1tiwMB-Pc",
+  authDomain: "chat-7099f.firebaseapp.com",
+  projectId: "chat-7099f",
+  storageBucket: "chat-7099f.appspot.com",
+  messagingSenderId: "883592581289",
+  appId: "1:883592581289:web:33fa4183014747607b99b8",
+  measurementId: "G-40B1B5DFJL"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
+
 function App() {
   return (
     <div className="App">
