46f2b6ac8373285662f154c99e9aed8a2dadcd3f
Nathan Brown <scottynate05@yahoo.com>
2023-02-20 15:43:16 -0800

    Built SignIn and SignOut functions

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -24,14 +24,43 @@ const firestore = firebase.firestore();
 
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  // Allow users to sign in with Google Auth
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+  // Chatroom for Authed users
 }
 
 export default App;
