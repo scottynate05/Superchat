4cd64dafedcae39ff4a5e9f43004622dd30d8a91
Nathan Brown <scottynate05@yahoo.com>
2023-02-20 17:51:46 -0800

    small changes

--- superchat/functions/index.js
+++ superchat/functions/index.js
@@ -1,6 +1,5 @@
 const functions = require("firebase-functions");
 const Filter = require('bad-words');
-
 const admin = require('firebase-admin');
 admin.initializeApp();
 
@@ -20,7 +19,17 @@ exports.detectEvilUsers = functions.firestore
             await db.collection('banned').doc(uid).set({});
         }
 
-    })
+        const userRef = db.collection('users').doc(uid)
+
+        const userData = (await userRef.get()).data();
+
+        if (userData.msgCount >= 7) {
+            await db.collection('banned').doc(uid).set({});
+        } else {
+            await userRef.set({ msgCount: (userData.msgCount || 0) + 1 })
+        }
+
+    });
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
