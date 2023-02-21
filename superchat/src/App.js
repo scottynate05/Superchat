43138aec108b05a8fec7408c3a212820a068c339
Nathan Brown <scottynate05@yahoo.com>
2023-02-20 16:35:51 -0800

    Set state management for form, and payload for message sent to server.

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -66,11 +66,34 @@ function ChatRoom() {
 
   const [messages] = useCollectionData(query, {idField: 'id'});
 
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+
+    e.preventDefault();
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    });
+    setFormValue('');
+  }
+
   return (
     <>
       <div>
         {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
       </div>
+      <form onSubmit={sendMessage}>
+
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+
+        <button type='submit'>Submit</button>
+
+      </form>
     </>
   )
 }
@@ -78,9 +101,14 @@ function ChatRoom() {
 function ChatMessage(props) {
   const { text, uid } = props.message;
 
-  
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
-  return <p>{text}</p>
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} />
+      <p>{text}</p>
+    </div>
+  )
 }
 
 export default App;
