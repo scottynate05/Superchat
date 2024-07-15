0f3e7ee0f61777d973375129178d5807927ff1c4
Nathan Brown <45585802+scottynate05@users.noreply.github.com>
2024-07-14 23:21:59 -0700

    Update App.js
    Edited name

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Welcome to CultChat</h1>
+        <h1>Welcome to SuperChat</h1>
         <SignOut />
       </header>
 
